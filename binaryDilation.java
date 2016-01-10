8c922bd9e15b608322d83615f81aadfe4a543e78
Tanaya Asnani <tasnani@umass.edugit config --global user.name Tanaya Asnani>
2016-01-10 00:28:50 -0500

    tried to check smallest contour area to identify page number, applied changes in different color in red to the blueMat matrix; does not work as; small contours area subsequently small and a page number is made up of a certain size of these smalll contours; will have to try newer approach or find mean of the contours making up the page number or use contours at the end of the MatOfPoints ArrayList.

--- binaryDilation.java
+++ binaryDilation.java
@@ -31,7 +31,10 @@ import javax.imageio.ImageIO;
 
 public class binaryDilation {
 
-	
+//int smallestContourLoc=0;
+Mat binaryMat;
+Mat blueMat;
+final List<MatOfPoint> contours = new ArrayList<>();
 	public BufferedImage dilate(BufferedImage bi) throws IOException{
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		BufferedImage convertImage=new BufferedImage(bi.getWidth(),bi.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
@@ -43,29 +46,48 @@ public class binaryDilation {
 		mat.put(0,0 , data);
 		Mat grayscaleMat=mat.clone();
 		Imgproc.cvtColor(mat, grayscaleMat, Imgproc.COLOR_BGR2GRAY);
-		Mat binaryMat=grayscaleMat.clone();
-		Imgproc.threshold(grayscaleMat, binaryMat, 50, 255, Imgproc.THRESH_BINARY);
+		 binaryMat=grayscaleMat.clone();
+		Imgproc.threshold(grayscaleMat, binaryMat, 70, 255, Imgproc.THRESH_BINARY);
 		
-		final List<MatOfPoint> contours = new ArrayList<>();
+		
 	    final Mat hierarchy = new Mat();
 	    Imgproc.findContours(binaryMat, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        Mat blueMat=mat.clone();
+	    //double smallestcontourArea=Double.MAX_VALUE;
+	    //int smallestContourLoc=0;
+        blueMat=mat.clone();
+        boolean oriented=false;
+       // System.out.println("double max value"+smallestcontourArea);
        
         for(int i=0;i<contours.size();i++){
         	Imgproc.drawContours(blueMat, contours, i, new Scalar(0, 255,0 ), -1);
         	
+        	//if( Imgproc.contourArea(contours.get(i),oriented)<smallestcontourArea && Imgproc.contourArea(contours.get(i),oriented)!=0.0 ){
+        		//smallestcontourArea=Imgproc.contourArea(contours.get(i));
+        		//smallestContourLoc=i;
+        		
+        		
+        	//}
         }
+        
+        
+        //System.out.println("smallest contour area:"+smallestcontourArea);
+       // System.out.println("smallestContourLoc:"+smallestContourLoc);
+       
         Mat grayscaleMat2=new Mat();
-        
-        
+       
+   
         
         
         Imgproc.cvtColor(blueMat,grayscaleMat2,Imgproc.COLOR_BGR2GRAY);
         Imgproc.threshold(grayscaleMat2, binaryMat, 35, 255, Imgproc.THRESH_BINARY);
-   	    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,5));
+   	    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,5
+   	    		));
    	    Imgproc.dilate(binaryMat, binaryMat, dilateElement);
+   	    
 		
 		
+		//Imgproc.drawContours(blueMat, contours, smallestContourLoc, new Scalar(0, 0,255 ), -1);
+    	
          
 		MatOfByte bytemat = new MatOfByte();
 		Highgui.imencode(".PNG", binaryMat, bytemat);
@@ -78,5 +100,14 @@ public class binaryDilation {
 		
 		
 	}
+	/*
+	public Mat gotofindsmallBlob(){
+		findsmallBlob fsB=new findsmallBlob();
+		blueMat=fsB.blobCheck(blueMat, contours, smallestContourLoc);
+		return blueMat;
+		
+	}
+	*/
+	
 
 }
