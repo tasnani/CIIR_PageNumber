dc414cbe3245fc86dafd72be30a990adc9ce5283
Tanaya Asnani <tasnani@umass.edugit config --global user.name Tanaya Asnani>
2016-01-11 00:10:11 -0500

    same as previous.

--- binaryDilation.java
+++ binaryDilation.java
@@ -111,7 +111,7 @@ Mat binaryMat;
         
         Imgproc.cvtColor(blueMat,grayscaleMat2,Imgproc.COLOR_BGR2GRAY);
         Imgproc.threshold(grayscaleMat2, binaryMat, 35, 255, Imgproc.THRESH_BINARY);
-   	    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(7,7
+   	    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(6,6
    	    		));
    	    Imgproc.dilate(binaryMat, binaryMat, dilateElement);
    	    
