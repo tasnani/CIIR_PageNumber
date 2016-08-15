8557131df266106b6b73d24024379e88d10518ed
Tanaya Asnani <tasnani@umass.edugit config --global user.name Tanaya Asnani>
2016-08-15 16:35:33 -0400

    Added significant changes to Forward.java as well as Viterbi.java. Made it more OOP style than C style. Created and loaded sample data for both classes to run. Need to work 2 points described in the TODO list of Viterbi.java

--- Forward.java
+++ Forward.java
@@ -6,43 +6,89 @@ import java.util.ArrayList;
 import pagenumber.ExtractFeaturesBook.PageNumberCandidate;
 
 public class Forward {
+	
+public static class ViterbiCandidate{
+	String text;
+	int page;
+	double rankedScores;
+	public ViterbiCandidate(String t, int p, double score){
+		this.text=t;
+		this.page=p;
+		this.rankedScores=score;
+	}
+}
 ArrayList<ArrayList<Double>> forward;
 ArrayList<ArrayList<Double>> transitionProbabilities;
 ArrayList<ArrayList<Double>> rankedScores;
-ArrayList<ArrayList<PageNumberCandidate>> book;
-int candidateSize=0;
-public void calculateTotalCandidatesN(){
-candidateSize=0;
-for(int i=0;i<forward.size();i++){
-	candidateSize=candidateSize+forward.get(i).size();
-}
-}
+ArrayList<ArrayList<ViterbiCandidate>> book;
+ArrayList<ViterbiCandidate> allCandidates;
 
-public Double findSUM(int page, int candidate, int lowerBound ,int upperBound, ArrayList<ArrayList<Double>> forward, ArrayList<ArrayList<Double>> transitionProbabilities , ArrayList<ArrayList<Double>> rankedScores){
+int candidateSize=0;
+public void initialize(){
+	allCandidates.add(new ViterbiCandidate("1",0,0.9));
+	allCandidates.add(new ViterbiCandidate("e",0,0.4));
+	allCandidates.add(new ViterbiCandidate("k",0,0.3));
+	allCandidates.add(new ViterbiCandidate("f",1,0.5));
+	allCandidates.add(new ViterbiCandidate("2",1,0.7));
+	allCandidates.add(new ViterbiCandidate("c",1,0.3));
+	allCandidates.add(new ViterbiCandidate("3",2,0.9));
+	allCandidates.add(new ViterbiCandidate("err",2,0.2));
+	allCandidates.add(new ViterbiCandidate("p",2,0.3));
+	
+	int index=0;
+	for(int i=0;i<3;i++){
+		book.add(new ArrayList<ViterbiCandidate>());
+		for (int j=0;j<3;j++){
+			forward.get(i).add(j,0.0);
+			book.get(i).add(j,allCandidates.get(index));
+			rankedScores.get(i).add(j,allCandidates.get(index).rankedScores);
+			index++;
+			
+		}
+	}
+	
+	}
+
+
+
+public Double findSUM(int page, int candidate, int lowerBound ,int upperBound, ArrayList<ArrayList<Double>> forward, ArrayList<ArrayList<Double>>transitionProbabilities , ArrayList<ArrayList<Double>> rankedScores){
 	Double value=0.0;
 	for(int i=lowerBound;i<=upperBound; i++){
 		 value= value + forward.get(page-1).get(i) * transitionProbabilities.get(i).get(candidate) * rankedScores.get(page).get(candidate);
 	}
  return value;
 }
-public Double calculateForward(){
-for(int candidate=0;candidate<forward.get(0).size();candidate++){
+public void calculateForward(){
+      initialize();
+for(int candidate=0;candidate<book.get(0).size();candidate++){
 	forward.get(0).set(candidate, transitionProbabilities.get(0).get(candidate)*
 			rankedScores.get(0).get(candidate));
 }
 
-for(int page=1;page<forward.size();page++){
-	for(int candidate=0;candidate<candidateSize;candidate++){
-		forward.get(page).set(candidate, findSUM(page, candidate, 0,forward.get(page).size(),forward, transitionProbabilities, rankedScores));
+for(int page=1;page<book.size();page++){
+	for(int candidate=0;candidate<book.get(page).size();candidate++){
+		forward.get(page).set(candidate, findSUM(page, candidate, 0,book.get(page).size(),forward, transitionProbabilities, rankedScores));
 	}
 }
-int lastPage=forward.size()-1;
-int lastCandidate=forward.get(lastPage).size()-1;
-forward.get(lastPage).set(lastCandidate, findSUM(lastPage,lastCandidate,0,candidateSize,forward,transitionProbabilities,rankedScores));
-return forward.get(lastPage).get(lastCandidate);
+int lastPage=book.size()-1;
+int lastCandidate=book.get(lastPage).size()-1;
+forward.get(lastPage).set(lastCandidate, findSUM(lastPage,lastCandidate,0,book.get(lastPage).size(),forward,transitionProbabilities,rankedScores));
+ 
 
 }
+public ArrayList<ArrayList<Double>> returnForward(){
+	return forward;
+}
+public ArrayList<ArrayList<ViterbiCandidate>> returnBook(){
+	return book;
+}
 
+public ArrayList<ArrayList<Double>> returnRankedScores(){
+	return rankedScores;
+}
+public ArrayList<ArrayList<Double>> returnTransitionProbabilities(){
+	return transitionProbabilities;
+}
 
 }
 
--- Viterbi.java
+++ Viterbi.java
@@ -3,21 +3,26 @@ package pagenumber;
 import java.util.ArrayList;
 
 import pagenumber.ExtractFeaturesBook.PageNumberCandidate;
+import pagenumber.Forward.ViterbiCandidate;
 
 public class Viterbi {
 ArrayList<ArrayList<Double>> viterbiPages;
-ArrayList<ArrayList<PageNumberCandidate>> backpointer;
-ArrayList<ArrayList<PageNumberCandidate>> book;
+ArrayList<ArrayList<ViterbiCandidate>> backpointer;
+ArrayList<ArrayList<ViterbiCandidate>> book;
 ArrayList<ArrayList<Double>> forward;
 ArrayList<ArrayList<Double>> transitionProbabilities;
 ArrayList<ArrayList<Double>> rankedScores;
-int candidateSize;
-public void calculateTotalCandidatesN(){
-candidateSize=0;
-for(int i=0;i<viterbiPages.size();i++){
-	candidateSize=candidateSize+viterbiPages.get(i).size();
+public Viterbi(ArrayList<ArrayList<Double>> forward, ArrayList<ArrayList<ViterbiCandidate>> book, ArrayList<ArrayList<Double>> transitionProbabilties, ArrayList<ArrayList<Double>> rankedScores){
+	this.forward=forward;
+	this.book=book;
+	this.rankedScores=rankedScores;
+	this.transitionProbabilities=transitionProbabilities;
+	
 }
-}
+
+
+
+
 public Double findMAX(int page, int candidate, int lowerBound, int upperBound, ArrayList<ArrayList<Double>> viterbiPages, ArrayList<ArrayList<Double>> transitionProbabilities){
 	Double maximum=0.0;
 	for(int i=lowerBound;i<=upperBound;i++){
@@ -33,7 +38,7 @@ public void doViterbi(){
  for(int candidate=0;candidate<book.get(0).size();candidate++){
 	forward.get(0).set(candidate, transitionProbabilities.get(0).get(candidate)* 
 			rankedScores.get(0).get(candidate));
-	backpointer.get(0).set(candidate, new PageNumberCandidate(null, null));
+	backpointer.get(0).set(candidate, new ViterbiCandidate("", 0,0.0));
  }
  for(int page=1;page<book.size();page++){
 	 for(int candidate=0;candidate<book.get(page).size();candidate++){
@@ -42,11 +47,32 @@ public void doViterbi(){
  }
  int lastPage=book.size()-1;
  int lastCandidate=book.get(lastPage).size()-1;
- viterbiPages.get(lastPage).set(lastCandidate, findMAX(lastPage+1, lastCandidate,0,candidateSize,viterbiPages,transitionProbabilities));
+ viterbiPages.get(lastPage).set(lastCandidate, findMAX(lastPage+1, lastCandidate,0,book.get(lastPage).size(),viterbiPages,transitionProbabilities));
  
  
 }
 
+public static void main(String args[]){
+	ArrayList<ArrayList<ViterbiCandidate>> book;
+	ArrayList<ArrayList<Double>> rankedScores;
+	ArrayList<ArrayList<Double>> forward;
+	ArrayList<ArrayList<Double>> transitionProbabilities;
+	Forward f=new Forward();
+	f.calculateForward();
+	book=f.returnBook();
+	rankedScores=f.returnRankedScores();
+	transitionProbabilities=f.returnTransitionProbabilities();
+	forward=f.returnForward();
+	
+	Viterbi v=new Viterbi(forward,book,transitionProbabilities, rankedScores);
+	
+	/* TODO
+	 * Research new function that calculates transition probabilities.
+	 * Fix arg max at the end of the Viterbi pseudocode. It should return the domain(Viterbi Candidates) rather than the probability values. 
+	 */
+	
+   
+}
 
 
 }
